eb9d8d04400212fde1ded3f93080e3bae6cfd9d3
rising-star92 <ryan.service.1001@gmail.com>
2023-08-14 17:50:55 +0700

    Refactor FormSFTP and NewSFTPContainer: Remove unnecessary directory inputs

--- src/app/(withHeader)/sftp/create/containers/index.tsx
+++ src/app/(withHeader)/sftp/create/containers/index.tsx
@@ -36,14 +36,7 @@ const NewSFTPContainer = ({ detail }: { detail?: SFTP }) => {
       retailer: null,
       sftp_host: '',
       sftp_username: '',
-      sftp_password: '',
-      purchase_orders_sftp_directory: '',
-      acknowledgment_sftp_directory: '',
-      confirm_sftp_directory: '',
-      inventory_sftp_directory: '',
-      invoice_sftp_directory: '',
-      return_sftp_directory: '',
-      payment_sftp_directory: ''
+      sftp_password: ''
     };
   }, []);
 
