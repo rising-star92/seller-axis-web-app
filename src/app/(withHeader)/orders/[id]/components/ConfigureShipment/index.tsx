f4d2b9c73202506fd2705a48c62cf1d76c7adeda
rising-star92 <ryan.service.1001@gmail.com>
2023-10-06 14:54:12 +0700

    fix: classification

--- src/app/(withHeader)/orders/[id]/components/ConfigureShipment/index.tsx
+++ src/app/(withHeader)/orders/[id]/components/ConfigureShipment/index.tsx
@@ -85,7 +85,7 @@ const ConfigureShipment = ({
   useEffect(() => {
     if (
       detail?.verified_ship_to?.status === 'VERIFIED' &&
-      detail?.ship_from?.classification === 'RESIDENTIAL'
+      detail?.verified_ship_to?.classification === 'RESIDENTIAL'
     ) {
       setValue('shipping_service', {
         label: dataHomeDelivery?.name || '',
@@ -94,7 +94,7 @@ const ConfigureShipment = ({
     }
   }, [
     dataHomeDelivery,
-    detail.ship_from?.classification,
+    detail.verified_ship_to?.classification,
     detail?.verified_ship_to?.status,
     setValue
   ]);
@@ -107,7 +107,7 @@ const ConfigureShipment = ({
           label: `${detail.batch.retailer?.default_carrier?.account_number}-${detail.batch.retailer.default_carrier?.service?.name}`
         },
         shipping_service:
-          detail?.ship_from?.classification === 'RESIDENTIAL'
+          detail?.verified_ship_to?.classification === 'RESIDENTIAL'
             ? {
                 label: dataHomeDelivery?.name || '',
                 value: dataHomeDelivery?.code || ''
