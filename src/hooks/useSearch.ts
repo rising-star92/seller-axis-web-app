31db85ece4421bf0df0740fd47f92c4b95f166cc
rising-star92 <ryan.service.1001@gmail.com>
2023-10-18 18:09:11 +0700

    feat: add search, filter to url param

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,19 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 import useDebounce from './useDebounce';
 
 const useSearch = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const searchBy = searchParams.get('search');
+  const params = new URLSearchParams(searchParams);
+
   const [search, setSearch] = useState('');
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    params.set('search', e.target.value);
+    router.push(`${pathname}?${params}`);
   };
   const debouncedSearchTerm = useDebounce(search, 500);
+
+  useEffect(() => {
+    searchBy && setSearch(searchBy);
+  }, [searchBy]);
 
   return {
     search,
     debouncedSearchTerm,
     handleSearch,
-    setSearch,
+    setSearch
   };
 };
 
